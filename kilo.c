f9033a3c3ee198a8b1666f4acbffcddbe559eb18
c-D-a-d-a-m-s <176541932+c-D-a-d-a-m-s@users.noreply.github.com>
2025-01-18 18:51:31 -0600

    Step 10: Disable Ctrl+S and Ctrl+Q

--- kilo.c
+++ kilo.c
@@ -16,10 +16,7 @@ void enableRawMode()
     atexit(disableRawMode);
 
     struct termios raw = orig_termios;
-    raw.c_lflag &= ~(ECHO | ICANON);
-
-    tcgetattr(STDIN_FILENO, &raw);
-
+    raw.c_iflag &= ~(IXON);
     raw.c_lflag &= ~(ECHO | ICANON | ISIG);
 
     tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
